688d10ad961e9811dfedc4cba9815429cf4ebf06
sbramson <sambramson@gmail.com>
2017-07-30 15:16:58 -0400

    print board to console

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -5,11 +5,29 @@ using namespace std;
 const char LIVECELL = '#';
 const char DEADCELL = '.';
 
+
 /* Internal representation choices */
 const char BOARDSIZE = 20;
 
 /* Initial state choices */
 
+/* Output */
+
+void print_board(char (&board)[BOARDSIZE][BOARDSIZE]) // yes, can manipulate referenced data
+{
+	for (int i = 0; i < BOARDSIZE; i++)
+	{
+		for (int j = 0; j < BOARDSIZE; j++)
+		{
+			cout << board[i][j];
+		}
+		cout << endl;
+	}
+	
+	for (int j = 0; j < BOARDSIZE; j++)
+		cout << '='; // separate consecutive entire-board printouts
+	cout << endl;
+}
 
 int main()
 {
@@ -24,4 +42,6 @@ int main()
 			board[i][j] = DEADCELL;
 		}
 	}
+	
+	print_board(board);
 }
