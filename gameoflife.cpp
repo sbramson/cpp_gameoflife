0fb7e813db3d6e25e5fce151abb466e6867b1776
sbramson <sambramson@gmail.com>
2017-07-30 14:54:19 -0400

    ascii characters for live and dead cells, board size

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+/* Output choices */
+const char LIVECELL = '#';
+const char DEADCELL = '.';
+
+/* Internal representation choices */
+const char BOARDSIZE = 20;
+
+/* Initial state choices */
+
+
 int main()
 {
-	cout << "Hello, world!";
+	char board [BOARDSIZE][BOARDSIZE];
 }
