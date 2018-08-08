ed84e0856ca08cfaddc454c373082e11af16c637
Sam Bramson <sambramson@gmail.com>
2018-08-08 14:43:41 -0400

    cowboy coding that allocates and initializes a 2D array board, then prints it --- also took a stab at a load-board-from-file function

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -29,6 +29,52 @@ void print_board(char (&board)[BOARDSIZE][BOARDSIZE]) // yes, can manipulate ref
 	cout << endl;
 }
 
+void fatal(string msg)
+{
+	cout << msg << endl;
+	exit();
+}
+
+char[][] load_board_from_file(string filepath)
+{	
+	ifstream myfile;
+	myfile.open(filepath);
+	if (!myfile.is_open()) return null;
+	
+	int height;
+	int width;
+	
+	if (getline(myfile, line))
+		height = std::stoi(line);
+	else
+		fatal("Could not parse height of board from file " + filepath);
+	
+	if (getline(myfile, line))
+		width = std::stoi(line);
+	else
+		fatal("Could not parse width of board from file " + filepath);
+	
+	char result [height][width];
+	int currentRow = 0;
+	
+	while(getline(myfile, line))
+	{
+		if (line.length() < width)
+			fatal("Malformed input file: line " + currentRow + " does not match stated board width");
+
+		for (int i = 0; i < width; i++)
+		{
+			if (line[i] != LIVECELL && line[i] != DEADCELL)
+				fatal("Malformed input file: line " + currentRow + " contains non-cell character");
+
+			result[currentRow][i] = line[i];
+		}
+		currentRow++;
+	}
+	
+	return result;
+}
+
 int main()
 {
 	/* Internal representation */
